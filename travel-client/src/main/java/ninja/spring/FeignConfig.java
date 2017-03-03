d53c44b45152df9d99ad05bde7b9e9ebaef2530e
Christophe <christophe-f>
2017-03-02 21:39:47 -0500

    Enable Feign BASIC logs

---
+++ travel-client/src/main/java/ninja/spring/FeignConfig.java
@@ -0,0 +1,13 @@
+package ninja.spring;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+}
